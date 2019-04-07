88e8ca59fe68f8d98ed34444e84b12ae5da97fc1
Jan Rundt <jan.rundt@gmail.com>
2019-04-07 21:20:10 +0300

    test

---
+++ build/precache-manifest.37f12dcdec721501b02c86a4a6c228b6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "d5f253c81d811d3bdbe2",
+    "url": "/static/js/main.06dcfb0b.chunk.js"
+  },
+  {
+    "revision": "37cfae212e25180f959b",
+    "url": "/static/js/2.0b74813f.chunk.js"
+  },
+  {
+    "revision": "d5f253c81d811d3bdbe2",
+    "url": "/static/css/main.214d1108.chunk.css"
+  },
+  {
+    "revision": "9e93e794551c3578ca5af1be1ba6c758",
+    "url": "/index.html"
+  }
+];
